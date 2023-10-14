ac94e56ccd784adeaa29ca139a06fad5ae59c9e1
James Zhao <accounts.james.zhao@protonmail.com>
2023-10-13 22:24:37 -0700

    update web frontend

--- hamster-web-frontend/src/App.tsx
+++ hamster-web-frontend/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MantineProvider, SimpleGrid } from '@mantine/core';
+import '@mantine/core/styles.css';
+import './App.css';
+import { Graph } from './components/Graph/Graph';
+import { Shell } from './components/Shell/Shell';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider>
+      <Shell>
+        <SimpleGrid cols={{ base: 1, sm: 1, md: 2, lg: 2, xl: 3 }}>
+          <Graph />
+          <Graph />
+          <Graph />
+          <Graph />
+          <Graph />
+          <Graph />
+          <Graph />
+          <Graph />
+        </SimpleGrid>
+      </Shell>
+    </MantineProvider>
   )
 }
 
---
+++ hamster-web-frontend/src/components/Shell/Shell.tsx
@@ -0,0 +1,39 @@
+import { AppShell, Burger, Group, Skeleton, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { ReactNode } from 'react';
+
+interface ShellProps {
+  children?: ReactNode;
+}
+
+export function Shell({ children }: ShellProps) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: { base: 60, md: 70, lg: 80 } }}
+      navbar={{
+        width: { sm: 240, md: 280},
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Text size="xl" fw={700}>🐹 Hamster</Text>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
