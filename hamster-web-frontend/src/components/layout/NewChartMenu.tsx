543be0283816ec3d015feff0fb87f96afd2ce500
shatayu <shatayukulkarni@gmail.com>
2023-10-14 20:39:24 -0400

    Updated modal style

--- hamster-web-frontend/src/components/layout/NewChartMenu.tsx
+++ hamster-web-frontend/src/components/layout/NewChartMenu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, Text, Paper } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { IconArrowLeftTail } from '@tabler/icons-react';
 
 const NewChartMenu: React.FC = () => {
   const [opened, { open, close }] = useDisclosure();
@@ -8,50 +9,83 @@ const NewChartMenu: React.FC = () => {
   const [page, setPage] = useState(1);
 
   const handleButtonClick = (value: string) => {
-      console.log('test');
     setSelectedButton(value);
     setPage(2);
   };
 
-  const buttonTextStyle = { fontWeight: 700 } as React.CSSProperties;
-  const centerTextStyle = { textAlign: 'center', fontSize: '1.5rem' } as React.CSSProperties;
-  const paperPadding = { padding: '1rem' } as React.CSSProperties;
-  const backButtonStyle = { position: 'absolute', top: '1rem', left: '1rem' } as React.CSSProperties;
+  const titleStyle = { fontWeight: 700, fontSize: '1.5rem', textAlign: 'center' } as React.CSSProperties;
   const buttonStyle = {
-    display: 'block',
     width: '100%',
     marginBottom: '10px',
     backgroundColor: 'white',
     borderColor: 'lightgray',
-    color: 'black'
+    color: 'black',
+    textAlign: 'left',
+    padding: '10px',
+    height: '70px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center', // This will center the items vertically
+    alignItems: 'center', // This will center the items horizontally
+    borderRadius: '15px'    
+  } as React.CSSProperties;
+  const paperStyle = { padding: '1rem', height: '400px' } as React.CSSProperties;
+  const subtextStyle = { color: 'lightgray', marginTop: '5px', fontSize: '13px' } as React.CSSProperties;
+  const backButtonStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    color: 'white', // Changed color to white
+    backgroundColor: 'rgba(0, 0, 0, 0.6)', // Added a semi-transparent black background
+    padding: '5px 10px', // Added padding
+    borderRadius: '5px', // Rounded corners
+    cursor: 'pointer',
+    position: 'absolute',
+    top: '10px',
+    left: '10px'
+  } as React.CSSProperties;
+  
+
+  const buttonTextStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center', // ensures vertical centering
+    height: '100%', // takes the full height of the parent button
+    fontSize: '18px'
+  } as React.CSSProperties;
+  
+  const buttonSpacing = {
+    marginBottom: '10px', // Adjust as needed
   } as React.CSSProperties;
 
   return (
     <div>
       <Button onClick={open}>Open Modal</Button>
-
-      <Modal opened={opened} onClose={close} title="Make New Chart" size="xl">
+  
+      <Modal opened={opened} onClose={close} size="xl" centered>
+        <Text style={titleStyle}>Make New Chart</Text>
+  
         {page === 1 ? (
-          <Paper style={paperPadding}>
+          <Paper style={paperStyle}>
             {['Time Between', 'Log User-Inputted Value', 'Log Time', 'Custom SQL'].map((text, idx) => (
-              <Button key={idx} style={buttonStyle} onClick={() => handleButtonClick(text)}>
-                <Text size="xl" style={buttonTextStyle}>{text}</Text> <br />
-                <Text>subtext {idx + 1}</Text>
+              <Button key={idx} style={{ ...buttonStyle, ...buttonSpacing }} onClick={() => handleButtonClick(text)}>
+                <div style={buttonTextStyle}>
+                  <div style={{ fontWeight: 'bold' }}>{text}</div>
+                  <Text style={subtextStyle}>subtext {idx + 1}</Text>
+                </div>
               </Button>
             ))}
           </Paper>
         ) : (
-          <Paper style={paperPadding}>
-            <Button style={backButtonStyle} onClick={() => setPage(1)}>
-              Back
-            </Button>
-            <Text style={centerTextStyle}>{selectedButton}</Text>
+          <Paper style={paperStyle}>
+            <div onClick={() => {setPage(1)}}>Test Aliens</div>
+            <Text style={{ ...titleStyle, textAlign: 'center', marginTop: '100px' }}>{selectedButton}</Text>
           </Paper>
         )}
-
+  
         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
-          <span style={{ width: '10px', height: '10px', borderRadius: '50%', backgroundColor: page === 1 ? 'black' : 'gray', margin: '0 5px' }} onClick={() => setPage(1)}></span>
-          <span style={{ width: '10px', height: '10px', borderRadius: '50%', backgroundColor: page === 2 ? 'black' : 'gray', margin: '0 5px' }}></span>
+          <span style={{ width: '10px', height: '10px', borderRadius: '50%', backgroundColor: page === 1 ? 'black' : 'lightgray', margin: '0 5px' }} onClick={() => setPage(1)}></span>
+          <span style={{ width: '10px', height: '10px', borderRadius: '50%', backgroundColor: page === 2 ? 'black' : 'lightgray', margin: '0 5px' }}></span>
         </div>
       </Modal>
     </div>
