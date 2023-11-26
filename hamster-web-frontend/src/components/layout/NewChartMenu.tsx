04ff2e9c5404cce4f5c23c4de28b63697f042f3e
shatayu <shatayukulkarni@gmail.com>
2023-11-25 20:33:26 -0500

    Added second page of new chart menu

--- hamster-web-frontend/src/components/layout/NewChartMenu.tsx
+++ hamster-web-frontend/src/components/layout/NewChartMenu.tsx
@@ -1,78 +1,138 @@
 import React, { useState } from 'react';
-import { Button, Modal, Text, Paper } from '@mantine/core';
+import { Button, Modal, Text, Paper, TextInput } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+
+type Shortcut = {
+  title: string;
+  type: string;
+  subtext: string;
+  exampleLink: string
+}
 
 const NewChartMenu: React.FC = () => {
   const [opened, { open, close }] = useDisclosure();
-  const [selectedButton, setSelectedButton] = useState<string | null>(null);
+  const [selectedButton, setSelectedButton] = useState<Shortcut | null>(null);
   const [page, setPage] = useState(1);
+  const [newTemplateName, setTemplateName] = useState<string>('');
+  const [username, setUsername] = useState<string>('premelon');
 
-  const handleButtonClick = (value: string) => {
+  const onModalClose = () => {
+    setPage(1);
+    setSelectedButton(null);
+    setUsername('');
+    setTemplateName('');
+    close();
+  }
+
+  const handleButtonClick = (value: Shortcut) => {
     setSelectedButton(value);
     setPage(2);
   };
 
+  const createShortcut = (selectedButton: Shortcut, name: string, username: string) => {
+    console.log(name + ' ' + username);
+  }
+
   const titleStyle = { fontWeight: 700, fontSize: '1.5rem', textAlign: 'center' } as React.CSSProperties;
-const buttonStyle = {
-  width: '100%',
-  marginBottom: '10px',
-  backgroundColor: 'white',
-  borderColor: 'lightgray',
-  color: 'black',
-  textAlign: 'left',
-  padding: '10px',
-  height: '70px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center', // This will center the items vertically
-  alignItems: 'center', // This will center the items horizontally
-  borderRadius: '15px'    
-} as React.CSSProperties;
-const paperStyle = { padding: '1rem', height: '400px' } as React.CSSProperties;
-const subtextStyle = { color: 'lightgray', marginTop: '5px', fontSize: '13px' } as React.CSSProperties;  
+  const buttonStyle = {
+    width: '100%',
+    marginBottom: '10px',
+    backgroundColor: 'white',
+    borderColor: 'lightgray',
+    color: 'black',
+    textAlign: 'left',
+    padding: '10px',
+    height: '70px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center', // This will center the items vertically
+    alignItems: 'center', // This will center the items horizontally
+    borderRadius: '15px'    
+  } as React.CSSProperties;
+  const paperStyle = { padding: '1rem', height: '400px' } as React.CSSProperties;
+  const subtextStyle = { color: 'lightgray', marginTop: '5px', fontSize: '13px' } as React.CSSProperties;  
 
-const buttonTextStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center', // ensures vertical centering
-  height: '100%', // takes the full height of the parent button
-  fontSize: '18px'
-} as React.CSSProperties;
+  const buttonTextStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center', // ensures vertical centering
+    height: '100%', // takes the full height of the parent button
+    fontSize: '18px'
+  } as React.CSSProperties;
 
-const buttonSpacing = {
-  marginBottom: '10px', // Adjust as needed
-} as React.CSSProperties;
+  const buttonSpacing = {
+    marginBottom: '10px', // Adjust as needed
+  } as React.CSSProperties;
 
-const pageIconStyle = {
-  width: '10px',
-  height: '10px',
-  borderRadius: '50%',
-  margin: '5px 5px',
-  outline: '1px solid'
-}
+  const pageIconStyle = {
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    margin: '5px 5px',
+    outline: '1px solid'
+  }
+
+  // ['Time Between', 'Log User-Inputted Value', 'Log Time', 'Custom SQL']
+  const shortcutTypes: Array<Shortcut> = [
+    {
+      title: 'Log Time',
+      type: 'LOG_TIME',
+      subtext: 'What time did you do something?',
+      exampleLink: 'https://www.icloud.com/shortcuts/3c6586a381d24b4e9579ed64c494c033',
+    }
+  ];
 
   return (
     <div>
       <Button onClick={open}>Open Modal</Button>
-      <Modal opened={opened} onClose={close} size="xl" centered>
+      <Modal opened={opened} onClose={onModalClose} size="xl" centered>
         <div style={{ overflowX: 'hidden' }}>
-          <Text style={titleStyle}>Make New Chart</Text>
+          {page === 1 && selectedButton != null ? <Text style={titleStyle}>Make New Chart</Text> : <Text style={{ ...titleStyle, textAlign: 'center'}}>{selectedButton?.title}</Text>}
 
-          {page === 1 ? (
+          {page === 1 || selectedButton == null ? (
             <Paper style={{ ...paperStyle }}>
-              {['Time Between', 'Log User-Inputted Value', 'Log Time', 'Custom SQL'].map((text, idx) => (
-                <Button key={idx} style={{ ...buttonStyle, ...buttonSpacing }} onClick={() => handleButtonClick(text)}>
+              {shortcutTypes.map((shortcut, idx) => (
+                <Button key={idx} style={{ ...buttonStyle, ...buttonSpacing }} onClick={() => handleButtonClick(shortcut)}>
                   <div style={buttonTextStyle}>
-                    <div style={{ fontWeight: 'bold' }}>{text}</div>
-                    <Text style={subtextStyle}>subtext {idx + 1}</Text>
+                    <div style={{ fontWeight: 'bold' }}>{shortcut.title}</div>
+                    <Text style={subtextStyle}>{shortcut.subtext}</Text>
                   </div>
                 </Button>
               ))}
             </Paper>
           ) : (
             <Paper style={{ ...paperStyle }}>
-              <Text style={{ ...titleStyle, textAlign: 'center', marginTop: '100px' }}>{selectedButton}</Text>
+              <TextInput
+                label='Enter your username here'
+                placeholder='premelon'
+                value={username}
+                onChange={(event) => setUsername(event.currentTarget.value.replace(' ', ''))}
+              />
+              <TextInput
+                label='Name your new shortcut'
+                placeholder='Enter name here, no spaces'
+                value={newTemplateName}
+                onChange={(event) => setTemplateName(event.currentTarget.value.replace(' ', ''))}
+              />
+              <Text>
+                <ol>
+                  <li>Download the <a href={selectedButton?.exampleLink}>example shortcut</a>.</li>
+                  <li>Change the username to {username.length > 0 ? <b>{username}</b> : 'your username'}.</li>
+                  <li>Change the eventName to {newTemplateName.length > 0 ? <b>{newTemplateName}.</b> : 'the name you choose above.'}</li>
+                  <li>Change the data field to be anything you want.</li>
+                </ol>
+                </Text>
+                <Button
+                  variant="filled"
+                  disabled={newTemplateName.length === 0 || selectedButton == null}
+                  onClick={() => {
+                    if (selectedButton != null && newTemplateName.length > 0) {
+                        createShortcut(selectedButton, newTemplateName, username);
+                      }
+                    }
+                  }
+                >Create Shortcut</Button>
             </Paper>
           )}
 
