c26cb8c69847ef5c5eb85e9050f1132f1dfd300b
shatayu <shatayukulkarni@gmail.com>
2024-01-23 22:38:29 -0500

    added getChartsAPI to frontend

--- hamster-web-frontend/src/App.tsx
+++ hamster-web-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider, SimpleGrid } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 import { Shell } from './components/layout/Shell';
 import { Visualization } from './components/visualization/Visualization';
@@ -10,6 +11,20 @@ function App() {
   const [userName, setUserName] = useState('james_c137');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [visualizations, setVisualizations] = useState(['weight', 'workout', 'calories', 'creatine', 'smth else']);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://qiqp6ejx2c.execute-api.us-east-1.amazonaws.com/prod/charts?ownerId=test');
+        // Assuming the response contains an array of visualizations
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this runs once on mount
 
   const renderCharts = () => {
     const charts: ReactNode[] = [];
