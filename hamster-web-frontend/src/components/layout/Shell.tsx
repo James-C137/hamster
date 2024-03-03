9f9924b105796c7ffec6a3f341ad6fc6c2d71e86
shatayu <shatayukulkarni@gmail.com>
2024-03-03 14:41:02 -0500

    removed navbar

--- hamster-web-frontend/src/components/layout/Shell.tsx
+++ hamster-web-frontend/src/components/layout/Shell.tsx
@@ -27,14 +27,6 @@ export function Shell({ children }: ShellProps) {
           <NewChartMenu />
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
-      </AppShell.Navbar>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
   );
