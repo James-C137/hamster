171d9e8828328cc429799d4bf3f7c1e8083b5811
shatayu <shatayukulkarni@gmail.com>
2024-03-03 16:33:31 -0500

    wired up actual data in

--- hamster-web-frontend/src/components/chart/ChartFactory.tsx
+++ hamster-web-frontend/src/components/chart/ChartFactory.tsx
@@ -4,10 +4,15 @@ import { chartTypesParser } from './ChartConstants';
 import { LineChart, lineChartDataParser } from './LineChart';
 import DateTimeScatterPlot from './DateTimeScatterPlot';
 
+const dataPointSchema = z.object({
+  x: z.string(), // Expecting a date string for x
+  y: z.string()  // Expecting a time string for y
+});
+
 export const chartFactoryPropsParser = z.object({
   title: z.string(),
   type: chartTypesParser,
-  data: z.any()
+  data: z.array(dataPointSchema)
 });
 
 export type IChartFactoryProps = z.infer<typeof chartFactoryPropsParser>;
@@ -17,18 +22,17 @@ export type IChartFactoryProps = z.infer<typeof chartFactoryPropsParser>;
  */
 export function ChartFactory(props: IChartFactoryProps) {
   props = chartFactoryPropsParser.parse(props);
+  console.log('props');
+  console.log(props);
+  console.log(props.data.map(data => data.x))
 
   const getChart = (props: IChartFactoryProps) => {
     switch (props.type) {
       case 'empty':
         return null;
       case 'line':
-        // return <LineChart data={lineChartDataParser.parse([{
-        //   id: props.title,
-        //   data: props.data
-        // }])} />;
-        const dates = ['3/3/2024', '3/4/2024', '3/5/2024'];
-        const times = ['3:27:26 PM', '4:30:00 PM', '2:15:45 PM'];
+        const dates = props.data.map(data => data.x);
+        const times = props.data.map(data => data.y);
 
         return <DateTimeScatterPlot x={dates} y={times} />
     }
