d76e6db255bcbd4c21fc9ae428c426434088e575
shatayu <shatayukulkarni@gmail.com>
2024-03-04 22:33:25 -0500

    hard coding limitations in SQL on the number of rows

--- logs-service-api/src/database-entities/logs/QueryGenerator.ts
+++ logs-service-api/src/database-entities/logs/QueryGenerator.ts
@@ -3,10 +3,10 @@ import { QueryType } from './QueryType'
 export function logQuery (queryType: QueryType, username: string, eventName: string): string {
   switch (queryType) {
     case 'LOG_TIME':
-      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}'`
+      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}' AND ts > CURRENT_TIMESTAMP - INTERVAL '90 days' LIMIT 1000000`
     case 'QUANTITY':
-      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}'`
+      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}' AND ts > CURRENT_TIMESTAMP - INTERVAL '90 days' LIMIT 1000000`
     default:
-      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}'`
+      return `SELECT * FROM logs WHERE username = '${username}' AND eventName = '${eventName}' AND ts > CURRENT_TIMESTAMP - INTERVAL '90 days' LIMIT 1000000`
   }
 }
